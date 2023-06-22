cbe7428185defd8f5e901c5a42678133024d52cc
lh5844 <lh5844rit@gmail.com>
2023-06-22 10:46:14 -0400

    removed extra spacing

--- app.py
+++ app.py
@@ -53,12 +53,10 @@ def experience():
     return jsonify({})
 
 
-
 @app.route('/resume/experience/<int:id>', methods = ['GET'])
 def get_experience(id):
     experience = data["experience"][id]
     return jsonify(experience)
-
 
 
 @app.route('/resume/education', methods=['GET', 'POST'])
