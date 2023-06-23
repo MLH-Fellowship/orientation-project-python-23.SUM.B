781063779b31c5fbfeea16ac72e52e39d9d82f69
Mubaraq Sani <{ID}+{username}@users.noreply.github.com>
2023-06-23 10:58:02 +0100

    test cases for new routes

--- test_pytest.py
+++ test_pytest.py
@@ -72,3 +72,49 @@ def test_skill():
 
     response = app.test_client().get('/resume/skill')
     assert response.json[item_id] == example_skill
+
+def test_contact():
+    '''
+    Add a new contact and then get all contacts. 
+    
+    Check that it returns the new contact in that list
+    '''
+    example_contact = {
+        "name": "John Doe",
+        "phone": "1234567890",
+        "email": "johndoe@gmail.com"}
+    
+    post_response = app.test_client().post('/resume/contact', json=example_contact)
+    assert post_response.status_code == 200
+
+    get_response = app.test_client().get('/resume/contact')
+    assert get_response.status_code == 200
+
+    
+    contact = get_response.json["contact"]
+    assert contact != {}
+    assert len(contact) > 0
+
+def test_contact_update():
+    '''
+    Add a new contact and then update it. 
+    
+    Check that it returns the updated contact
+    '''
+    example_contact = {
+        "name": "John Doe",
+        "phone": "1234567890",
+        "email": "johndoe@gmail.com"}
+    
+    update_contact = {
+        "name": "Jane Doe",
+        "phone": "0987654321",
+        "email": "johndoer@yahoo.com"}
+    
+    post_response = app.test_client().post('/resume/contact', json=example_contact)
+    assert post_response.status_code == 200
+
+    item_id = [c for c in post_response.json['contact']][0]['id']
+
+    put_response = app.test_client().put(f'/resume/contact/{item_id}', json=update_contact)
+    assert put_response.status_code == 200
